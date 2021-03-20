766278f05733c40250fe59d18cd3cebb4c61f2a5
Iancu Mihaela <46075693+MihaelaIancu@users.noreply.github.com>
2021-03-21 01:39:12 +0200

    Add files via upload

---
+++ lab 6/script - imdb.js
@@ -0,0 +1,26 @@
+//alert("yes");
+
+//var movieObj = [
+//    {
+//        "title" : "Parasite",
+//        "duration (min)" : 132,
+//        "cast" : ["actor 1", "actor 2", "actor 3"],
+//        "seen" : true
+//    },
+//    {
+//        "title" : "Rear Window",
+//        "duration (min)" : 112,
+//        "cast" : ["actor 11", "actor 22", "actor 33"],
+//        "seen" : true
+//    },
+//    {
+//        "title" : "Midsommar",
+//        "duration (min)" : 138,
+//        "cast" : ["actor 111", "actor 222", "actor 333"],
+//        "seen" : false
+//    }
+//];
+
+//"https://wallpapercave.com/wp/wp5264425.jpg";
+
+
---
+++ lab 6/script.js
@@ -0,0 +1,2 @@
+//alert("yes");
+//"https://i.pinimg.com/originals/fe/a9/fb/fea9fb227210fdb349846e970ab61551.png";
