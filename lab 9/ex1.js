758229f5a7c3a1509a48f2e47f1a17da74270c11
Iancu Mihaela <46075693+MihaelaIancu@users.noreply.github.com>
2021-04-15 19:03:03 +0300

    Update ex1.js

--- lab 9/ex1.js
+++ lab 9/ex1.js
@@ -1,1 +1,32 @@
 //ex1
+
+window.onload = function() {
+    
+    document.getElementById("op").addEventListener('change', function(event) {
+        
+    var x = document.getElementById("i1").value;//valoarea input-ului se ia imediat dupa incarcarea paginii si doar 1 singura data
+    var y = document.getElementById("i2").value;
+    var op = document.getElementById("op").value;
+    
+    var parag = "<p>" + x + " " + op + " " + y;
+    var result;   
+        
+        switch(op){
+            case "+":
+                result = parseInt(x) + parseInt(y);
+                break;
+            case "-":
+                result = parseInt(x) - parseInt(y);
+                break;
+            case "*":
+                result = parseInt(x) * parseInt(y);
+                break;
+        };
+        
+    parag += " = " + result;
+    parag += "</p>";
+    document.getElementById("operatii").innerHTML += parag;
+        
+    });
+   
+}
